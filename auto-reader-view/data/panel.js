0c03f3b6ea48400800adf5a7a87d04abc4eea978
Patrick Marchwiak <pd@marchwiak.com>
2016-02-27 14:28:35 -0800

    Address warnings - remove use of innerHTML and exclude more files from XPI

--- auto-reader-view/data/panel.js
+++ auto-reader-view/data/panel.js
@@ -8,24 +8,28 @@ self.port.on("panelOpened", function(state) {
   console.log("panelOpened event received");
   console.log(state);
   domain = state.domain;
-  if (state.enabled) {
-    setEnabledButtonState();
+  if (domain) {
+    if (state.enabled) {
+      setEnabledState(domain, btn, prompt);
+    }
+    else {
+      setDisabledState(domain, btn, prompt);
+    }
   }
   else {
-    setDisabledButtonState();
+    setUnsupportedState(btn, prompt);
   }
 });
 
 function submitClicked(event) {
   var btn = event.target;
-  var newVal = btn.value;
-  var isEnabled = (newVal == "Enable");
+  var isEnabled = (btn.value == "Enable");
 
   if (isEnabled) {
-    setEnabledButtonState();
+    setEnabledState(domain, btn, prompt);
   }
   else {
-    setDisabledButtonState();
+    setDisabledState(domain, btn, prompt);
   }
 
   self.port.emit("domainChange", {
@@ -34,14 +38,39 @@ function submitClicked(event) {
   });
 }
 
-function setEnabledButtonState() {
+function setEnabledState(domain, btn, prompt) {
+  btn.style.display = '';
   btn.value = "Disable";
-  prompt.innerHTML =
-     "<b>" + domain + "</b> will automatically open in Reader View.";
+  replacePromptText(prompt, "", domain,
+      " will automatically open in Reader View.");
 }
 
-function setDisabledButtonState() {
+function setDisabledState(domain, btn, prompt) {
+  btn.style.display = '';
   btn.value = "Enable";
-  prompt.innerHTML =
-     "Always open <b>" + domain + "</b> in Reader View?";
+  replacePromptText(prompt, "Always open ", domain, " in Reader View?")
+}
+
+function setUnsupportedState(btn, prompt) {
+  btn.style.display = 'none';
+  replacePromptText(prompt, "Reader View not available on this page.", "", "");
+}
+
+function replacePromptText(prompt, part1, domain, part2) {
+  // clear out prompt text
+  while (prompt.firstChild) prompt.removeChild(prompt.firstChild);
+
+  // TODO use a library / templating here instead
+
+  var text1 = document.createTextNode(part1);
+
+  var b = document.createElement("b");
+  var domainText = document.createTextNode(domain);
+  b.appendChild(domainText);
+
+  var text2 = document.createTextNode(part2);
+
+  prompt.appendChild(text1);
+  prompt.appendChild(b);
+  prompt.appendChild(text2);
 }
